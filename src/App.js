1613e1768d35598339f3283645d7137dfbe74b37
Giorgio-g97 <124620661+Giorgio-g97@users.noreply.github.com>
2023-11-26 17:04:01 +0100

    Fix shadow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import ProtectedRoutes from "./middlewares/ProtectedRoutes";
 import NavbarIndie from './components/Navbarindie'
